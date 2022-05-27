9a6d54af822631a96feb195ff6b4faf4635d9687
Nipuna Athelli <nipunaathelli@Nipunas-MacBook-Pro.local>
2022-05-27 01:26:51 -0700

    day 6

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const ALGOLIA_CLIENT = "6be0576ff61c053d5f9a3225e2a90f76";
 
-export const MOVIE_NAME = "Prasthanam";
+export const MOVIE_NAME = "Govinda Govinda";
 
-export const dayCount = 3;
+export const dayCount = 4;
