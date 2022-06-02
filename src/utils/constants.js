0388f2846e85b845137dbe0bfe2a46b9b2bf5188
Nipuna Athelli <nipunaathelli@Nipunas-MacBook-Pro.local>
2022-06-02 06:05:35 +0530

    make golimar also correct

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,8 @@ export const ALGOLIA_CLIENT = "6be0576ff61c053d5f9a3225e2a90f76";
 
 export const MOVIE_NAME = "Golimaar";
 
+export const ALTERNATE_MOVIE_NAME = "Golimar";
+
 export const greenSquare = "🟩";
 
 export const redSquare = "🟥";
