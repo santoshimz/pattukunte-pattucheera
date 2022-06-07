506acb947223f83ada63f6239c5c9597c583a134
Nipuna Athelli <nipunaathelli@Nipunas-MacBook-Pro.local>
2022-06-07 22:02:41 +0530

    [skip ci] Day 16 fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const ALGOLIA_CLIENT = "6be0576ff61c053d5f9a3225e2a90f76";
 
-export const MOVIE_NAME = "Bharat ane nenu";
+export const MOVIE_NAME = "Bharat Ane Nenu";
 
 export const ALTERNATE_MOVIE_NAME = MOVIE_NAME;
 
