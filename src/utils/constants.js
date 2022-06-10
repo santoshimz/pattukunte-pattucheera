57d70dc249b98b267da4d2fc39cdfb7444978532
Pavan kalyan C <pavankalyan141640@gmail.com>
2022-06-10 18:23:29 +0530

    dummy commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -61,7 +61,8 @@ export function getTimeDifference(date) {
 }
 
 export function getDayCount() {
-  return Math.abs(getTimeDifference(new Date("2022-05-24T00:00:14.000Z")).days);
+  return 20;
+  //return Math.abs(getTimeDifference(new Date("2022-05-24T00:00:14.000Z")).days);
 }
 export const intialGuessDistribution = new Array(MAX_ATTEMPTS).fill().reduce((acc, _, index) => {
   acc[index + 1] = 0;
