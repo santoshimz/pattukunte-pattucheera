f5d05a787deefc7b1423b2755e897e828a9e9a16
Pavan kalyan C <pavankalyan141640@gmail.com>
2022-06-12 15:21:15 +0530

    removed algolia and static images to reduce bundle size

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,3 @@
-export const ALGOLIA_CLIENT = "6be0576ff61c053d5f9a3225e2a90f76";
-
-export const MOVIE_NAME = "Ghatotkachudu";
-
-export const ALTERNATE_MOVIE_NAME = MOVIE_NAME;
-
 export const greenSquare = "🟩";
 
 export const redSquare = "🟥";
