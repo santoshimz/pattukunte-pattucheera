fb23a15e728c38d5dc70b5c771b91dfdd3ef8b4e
Pavan kalyan C <pavankalyan141640@gmail.com>
2022-06-16 18:17:57 +0530

    code cleanup

--- src/components/banner.js
+++ src/components/banner.js
@@ -4,8 +4,7 @@ import { bannerStyles } from "../styles/bannerStyles";
 const Banner = () => {
   const [bannerText, setBannerText] = React.useState(process.env.REACT_APP_BANNER);
   return (
-    bannerText &&
-    true && (
+    bannerText && (
       <div style={bannerStyles.banner} className="row">
         <div className="col-xs-10 text-center">
           <span style={bannerStyles.infoIcon} className="material-symbols-outlined">
