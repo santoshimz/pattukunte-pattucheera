f39aec96a59b2b3fea75280160829c165b96eb8e
Pavan kalyan C <pavankalyan141640@gmail.com>
2022-06-30 13:11:54 +0530

    time travel initial poc

---
+++ src/pages/Home.js
@@ -0,0 +1,142 @@
+import React from "react";
+import "../styles/App.css";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+import PropTypes from "prop-types";
+import { customStyles } from "../styles/styles";
+import {
+  GAME_STATUS,
+  getDayCount,
+  intialGuessDistribution,
+  isProduction
+} from "../utils/constants";
+
+import Game from "../components/Game";
+import Stats from "../components/Stats";
+import ImagesContainer from "../components/ImagesContainer";
+import statsLogo from "../assets/stats.svg";
+import rulesLogo from "../assets/rules.svg";
+import RulesModal from "../components/RulesModal";
+import Banner from "../components/banner";
+import Footer from "../components/Footer";
+import { Outlet } from "react-router-dom";
+
+const Home = ({ timeTravelDate }) => {
+  console.log(timeTravelDate);
+  const [currentIndexFromStorage, setCurrentIndexFromStorage] = useLocalStorage("currentIndex", 1);
+  const [buttonLogic, setButtonLogic] = React.useState(false);
+  const [currentIndexFromButton, setCurrentIndexFromButton] =
+    React.useState(currentIndexFromStorage);
+  const [currentGuesses, setCurrentGuesses] = useLocalStorage("currentGuesses", "");
+  const [gameStatus, setGameStatus] = useLocalStorage("gameStatus", GAME_STATUS.RUNNING);
+  const [day, setDay] = useLocalStorage("day", 1);
+  const [openStatsModal, setOpenStatsModal] = React.useState(false);
+  const [openRulesModal, setOpenRulesModal] = React.useState(false);
+  const [movie, setMovie] = React.useState("");
+  const [contributor, setContributor] = React.useState("");
+  const [guessDistribution, setGuessDistribution] = useLocalStorage(
+    "guessDistribution",
+    JSON.stringify(intialGuessDistribution)
+  );
+
+  const initialStats = {
+    gamesPlayed: 0,
+    gamesWon: 0,
+    currentStreak: 0,
+    maxStreak: 0
+  };
+
+  const [stats, setStats] = useLocalStorage("stats", JSON.stringify(initialStats));
+
+  const statsObj = React.useMemo(() => {
+    return typeof stats === "string" ? JSON.parse(stats) : stats;
+  }, [stats]);
+
+  React.useEffect(() => {
+    const dayCount = timeTravelDate ? timeTravelDate : getDayCount();
+    fetch(`${process.env.REACT_APP_CDN_URL}${isProduction() ? "/" + dayCount : ""}/meta-data.json`)
+      .then((response) => response.json())
+      .then((json) => {
+        setMovie(json.movie);
+        setContributor(json.contributor);
+      })
+      .catch((error) => console.log(error));
+    if (day !== dayCount) {
+      setGameStatus(GAME_STATUS.RUNNING);
+      setDay(dayCount);
+      setCurrentGuesses("");
+      setCurrentIndexFromStorage(1);
+    }
+  }, [day, setCurrentGuesses, setCurrentIndexFromStorage, setDay, setGameStatus]);
+  return (
+    <div style={customStyles.backgroundStyle}>
+      {process.env.REACT_APP_BANNER && <Banner></Banner>}
+      <div style={customStyles.headerStyle}>Pattukunte Pattucheera</div>
+      <span style={customStyles.statsStyle}>
+        <img
+          className="stats-icon"
+          onClick={() => setOpenStatsModal(true)}
+          src={statsLogo}
+          alt="stats"
+        />
+        <img
+          className="rules-icon"
+          onClick={() => setOpenRulesModal(true)}
+          src={rulesLogo}
+          alt="rules"
+        />
+      </span>
+      <Stats
+        openStatsModal={openStatsModal}
+        setOpenStatsModal={setOpenStatsModal}
+        statsObj={statsObj}
+        guessData={JSON.parse(guessDistribution)}
+      />
+      <RulesModal openRulesModal={openRulesModal} setOpenRulesModal={setOpenRulesModal} />
+      <div style={customStyles.column}>
+        <div />
+        <ImagesContainer
+          buttonLogic={buttonLogic}
+          setButtonLogic={setButtonLogic}
+          currentIndexFromButton={currentIndexFromButton}
+          currentIndexFromStorage={currentIndexFromStorage}
+          setCurrentIndexFromButton={setCurrentIndexFromButton}
+          gameStatus={gameStatus}
+          dayCount={day}
+        />
+        <Game
+          currentIndex={currentIndexFromStorage}
+          setCurrentIndex={setCurrentIndexFromStorage}
+          currentIndexFromButton={currentIndexFromButton}
+          setCurrentIndexFromButton={setCurrentIndexFromButton}
+          guessDistribution={guessDistribution}
+          setGuessDistribution={setGuessDistribution}
+          currentGuesses={currentGuesses}
+          setCurrentGuesses={setCurrentGuesses}
+          gameStatus={gameStatus}
+          setGameStatus={setGameStatus}
+          day={day}
+          setDay={setDay}
+          setStats={setStats}
+          stats={stats}
+          gameStats={statsObj}
+          movie={movie}
+          setOpenStatsModal={setOpenStatsModal}
+          contributor={contributor}
+        />
+      </div>
+      <Footer />
+      <Outlet />
+    </div>
+  );
+};
+
+Home.propTypes = {
+  searchState: PropTypes.object,
+  createURL: PropTypes.func,
+  onSearchStateChange: PropTypes.func,
+  movie: PropTypes.string,
+  setMovie: PropTypes.func,
+  timeTravelDate: PropTypes.number
+};
+
+export default Home;
---
+++ src/pages/TimeTravel.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { getTimeDifference, getDateTimeInUTC, getDayCount } from "../utils/constants";
+import Home from "./Home";
+
+const TimeTravel = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [timeTravelDate, setTimeTravelDate] = React.useState(getDayCount(new Date()));
+  // eslint-disable-next-line no-unused-vars
+  const handleChangeFromDate = (event) => {
+    const selectedDate = event.target.value;
+    let diff = getTimeDifference(
+      getDateTimeInUTC(new Date(selectedDate)),
+      new Date("2022-05-22T18:30:00.000Z")
+    );
+    console.log(diff.days);
+    setTimeTravelDate(diff.days);
+  };
+
+  return (
+    <div className="text-white">
+      <h1 className="text-primary col-12 text-center">You have been time travelled!</h1>
+      <div className="col-6 form-group m-auto p-2">
+        Date
+        <input
+          className="form-control"
+          min="2022-05-23"
+          max={new Date().toISOString().split("T")[0]}
+          onChange={handleChangeFromDate}
+          type="date"
+        />
+      </div>
+      <Home timeTravelDate={timeTravelDate} />
+    </div>
+  );
+};
+
+TimeTravel.propTypes = {};
+
+export default TimeTravel;
