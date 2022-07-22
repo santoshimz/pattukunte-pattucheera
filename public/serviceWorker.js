06266002ac8558641ae28b456726a12ba45af9d5
Veleti <svelet2@uic.edu>
2022-07-22 15:11:17 -0500

    Removed logs

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -6,7 +6,6 @@ const self = this;
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened cache");
       return cache.addAll(urlsToCache);
     })
   );
