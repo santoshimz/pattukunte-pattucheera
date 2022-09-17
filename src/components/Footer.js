56c7d9275db75e75ccb6a7a70a42659d963d1c16
YashwanthC <cyashwanthkumar@presidio.com>
2022-09-18 04:29:42 +0530

    Added external game link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,21 @@ const Footer = ({ theme }) => {
     <div
       style={theme === "dark" ? FooterStyles.footer : FooterStyles.footerLight}
       className="col-12 text-center">
+      <div className="mb-4 d-flex align-items-start justify-content-center">
+        <span className="material-symbols-outlined new-icon" style={{ fontSize: "2rem" }}>
+          new_releases
+        </span>
+        <span style={{ fontSize: "1.25rem" }} className="ml-1">
+          Loved <strong className="app-name">Pattukunte Pattucheera</strong>? Want to explore more
+          of our apps?{" "}
+          <a href="https://guess-the-meme.vercel.app/" rel="noreferrer" target="_blank">
+            click here!!
+            <span className="material-symbols-outlined" style={{ fontSize: "1rem" }}>
+              open_in_new
+            </span>
+          </a>
+        </span>
+      </div>
       <span className="banner-text">
         We are opensource now! Want to contribute questions? Submit images through this{" "}
         <a href={process.env.REACT_APP_FormsLink}>forms</a> link
