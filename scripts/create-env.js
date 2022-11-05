d8140d3a7e09d0c996e4b50b100fc5e0fd59a280
Pavan kalyan C <pavankalyan141640@gmail.com>
2022-11-05 15:38:35 +0530

    added fix to local debugging

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -2,6 +2,6 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV !== "production") {
   const fs = require("fs");
   fs.writeFileSync(
     ".env",
-    "REACT_APP_FormsLink=https://forms.gle/9rDLcRBUpcUb4FR89\nNODE_ENV=development\n REACT_APP_BANNER=\nREACT_APP_CDN_URL=./static"
+    "REACT_APP_FormsLink=https://forms.gle/9rDLcRBUpcUb4FR89\nNODE_ENV=development\n REACT_APP_BANNER=\nREACT_APP_CDN_URL=./static\nREACT_APP_S3=./static"
   );
 }
