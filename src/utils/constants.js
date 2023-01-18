f4a245e999cc4dd00f8276fed4e7066a9584dab6
Santosh Eppalapally <santoshimz@gmail.com>
2023-01-18 10:00:54 -0800

    Missing movies

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -130,12 +130,13 @@ export const missingMovies = [
   "Jai Bhim",
   "Roja",
   "Aawara",
-  "K.G.F: Chapter 2",
+  "KGF: Chapter 2",
   "Radhe Shyam",
   "Like, Share & Subscribe",
   "Most Eligible Bachelor",
   "College Don",
   "Darbar",
   "Happy Birthday",
-  "Viyyalavari Kayyalu"
+  "Viyyalavari Kayyalu",
+  "Mallanna"
 ];
